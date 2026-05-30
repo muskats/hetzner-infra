ea52274638534fd0eb6ef9943115b6606983f0c1
muskats <muskatsebestyen@gmail.com>
2026-05-30 16:01:34 +0200

    output + license

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "server_ipv6s" {
-  value = module.instance.server_ipv6s
+output "servers" {
+  value = [
+    for idx, ip in module.instance.server_ipv6s : {
+      name = "node-${idx}"
+      ipv6 = ip
+      role = idx == 0 ? "control-plane" : "worker"
+    }
+  ]
 }
