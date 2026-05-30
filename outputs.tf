135b425de0cc5483fd5d9aeb6a300e5817cfe859
muskats <muskatsebestyen@gmail.com>
2026-05-30 18:19:09 +0200

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "servers" {
+output "server_ipv4s" {
   value = [
     for idx, ip in module.instance.server_ipv4s : {
       name = "node-${idx}"
@@ -7,3 +7,14 @@ output "servers" {
     }
   ]
 }
+
+output "server_ipv6s" {
+  value = [
+    for idx, ip in module.instance.server_ipv6s : {
+      name = "node-${idx}"
+      ipv6 = ip
+      role = idx == 0 ? "control-plane" : "worker"
+    }
+  ]
+}
+#
